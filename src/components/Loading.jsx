961ba21671cbe1e836137ef5a44d67dd54c5b625
Carter Rink <carter.m.rink@gmail.com>
2023-11-29 13:52:50 -0500

    made loading screen responsive

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Loading() {
   return (
-    <div className="h-screen w-screen bg-slate-600 flex items-center justify-center absolute top-0 left-0 z-50 select-none text-white drop-shadow-lg text-center text-5xl font-PressStart2P-Regular">
+    <div className="h-screen w-screen bg-slate-600 flex items-center justify-center absolute top-0 left-0 z-50 select-none text-white drop-shadow-lg text-center text-2xl lg:text-5xl font-PressStart2P-Regular">
         Loading...
     </div>
   ) 
