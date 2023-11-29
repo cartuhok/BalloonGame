b1e8904e338f5086c3f5cb29b9248fe90c25ac25
Carter Rink <carter.m.rink@gmail.com>
2023-11-29 11:30:31 -0500

    Added loading screen

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Loading() {
+  return (
+    <div className="h-screen w-screen bg-slate-600 flex items-center justify-center absolute top-0 left-0 z-50 select-none text-white drop-shadow-lg text-center text-5xl font-PressStart2P-Regular">
+        Loading...
+    </div>
+  ) 
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from "react"
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Loading from "./components/Loading.jsx"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <Suspense fallback={null}>
+      <Suspense fallback={<Loading />}>
         <App />
       </Suspense>
   </React.StrictMode>
